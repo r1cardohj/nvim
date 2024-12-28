53cca773b96dd800bf958eb5f14f04db4436a08c
r1cardohj <2413302357@qq.com>
2024-12-28 21:38:43 +0800

    del lazygit.nvim update snacks.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -47,15 +47,16 @@ return {
 		"folke/snacks.nvim",
 		priority = 1000,
 		lazy = false,
+		---@type snacks.Config
 		opts = {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 			bigfile = { enabled = true },
-			dashboard = {
+      dashboard = {
 				preset = {
 					header = [[
-          "          ▀████▀▄▄              ▄█ ",
+    "          ▀████▀▄▄              ▄█ ",
     "            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ",
     "    ▄        █          ▀▀▀▀▄  ▄▀  ",
     "   ▄▀ ▀▄      ▀▄              ▀▄▀  ",
@@ -75,6 +76,65 @@ return {
 			scroll = { enabled = true },
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
+		},
+		keys = {
+			{
+				"<leader>n",
+				function()
+					Snacks.notifier.show_history()
+				end,
+				desc = "Notification History",
+			},
+			{
+				"<leader>gB",
+				function()
+					Snacks.gitbrowse()
+				end,
+				desc = "Git Browse",
+				mode = { "n", "v" },
+			},
+			{
+				"<leader>gb",
+				function()
+					Snacks.git.blame_line()
+				end,
+				desc = "Git Blame Line",
+			},
+			{
+				"<leader>gf",
+				function()
+					Snacks.lazygit.log_file()
+				end,
+				desc = "Lazygit Current File History",
+			},
+			{
+				"<leader>gl",
+				function()
+					Snacks.lazygit.log()
+				end,
+				desc = "Lazygit Log (cwd)",
+			},
+			{
+				"<leader>lg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "Lazygit",
+			},
+			{
+				"<leader>un",
+				function()
+					Snacks.notifier.hide()
+				end,
+				desc = "Dismiss All Notifications",
+			},
+			{
+				"<c-/>",
+				function()
+					Snacks.terminal()
+				end,
+				desc = "Toggle Terminal",
+			},
 		},
 	},
 }
