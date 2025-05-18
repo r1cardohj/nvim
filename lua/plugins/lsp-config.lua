85bdd309a9ca1d01c9444f26ffaa0439d692e1c2
r1cardohj <2413302357@qq.com>
2025-05-18 23:05:30 +0800

    feat: vue support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -65,6 +65,37 @@ return {
           end,
         },
       }
+      local mason_registry = require('mason-registry')
+      local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
+
+      lspconfig.ts_ls.setup({
+        init_options = {
+          plugins = {
+            {
+              -- Name of the TypeScript plugin for Vue
+              name = '@vue/typescript-plugin',
+
+              -- Location of the Vue language server module (path defined in step 1)
+              location = vue_language_server_path,
+
+              -- Specify the languages the plugin applies to (in this case, Vue files)
+              languages = { 'vue' },
+            },
+          },
+        },
+
+        -- Specify the file types that will trigger the TypeScript language server
+        filetypes = {
+          'typescript',    -- TypeScript files (.ts)
+          'javascript',    -- JavaScript files (.js)
+          'javascriptreact', -- React files with JavaScript (.jsx)
+          'typescriptreact', -- React files with TypeScript (.tsx)
+          'vue'            -- Vue.js single-file components (.vue)
+        },
+      })
+
+
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
